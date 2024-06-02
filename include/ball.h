f48cff4cae1cfcb59dc2ed9e8f295d63cd13ebb0
Michela Schibuola <michela.schibuola@gmail.com>
2024-06-02 13:46:35 +0200

    Ball and category

---
+++ include/ball.h
@@ -0,0 +1,32 @@
+// Author: Michela Schibuola
+
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core/mat.hpp>
+#include "category.h"
+
+#ifndef BALL_H
+#define BALL_H
+
+class Ball
+{
+    cv::Rect bbox_;
+    Category category_;
+    cv::Rect bbox_prec_;
+
+public:
+    Ball(cv::Rect bbox, Category category, cv::Rect bbox_prec) : bbox_(bbox), category_(category), bbox_prec_(bbox_prec) {}
+    Ball(cv::Rect bbox, Category category) : bbox_(bbox), category_(category), bbox_prec_(cv::Rect(0, 0, 0, 0)) {}
+    Ball(cv::Rect bbox) : bbox_(bbox), category_(background), bbox_prec_(cv::Rect(0, 0, 0, 0)) {}
+    Ball() : bbox_(cv::Rect(0, 0, 0, 0)), category_(background), bbox_prec_(cv::Rect(0, 0, 0, 0)) {}
+
+    cv::Rect getBbox() const;
+    Category getCategory() const;
+    cv::Rect getBbox_prec() const;
+
+    void setBbox(cv::Rect bbox);
+    void setCategory(Category category);
+    void setBbox_prec(cv::Rect bbox_prec);
+};
+
+#endif // BALL_H

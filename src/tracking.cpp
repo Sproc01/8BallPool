c21e86d3a40d97c69ae8c0fa3c92c5d9e198eb53
Alberto Pasqualetto <alberto.pasqualetto.2@studenti.unipd.it>
2024-06-04 23:54:17 +0200

    Tracking first impl

---
+++ src/tracking.cpp
@@ -0,0 +1,62 @@
+// Author: Alberto Pasqualetto
+
+#include "tracking.h"
+#include "ball.h"
+#include <opencv2/tracking.hpp>
+
+
+
+void BallTracker::createTrackers() {
+	for (unsigned short i = 0; i < ballsVec_.size(); i++) {
+		cv::Ptr<cv::Tracker> tracker = cv::TrackerCSRT::create();    //parameters go here if necessary
+		ballTrackers_.push_back(tracker);
+	}
+
+	ballTrackers_.shrink_to_fit();
+}
+
+
+BallTracker::BallTracker(const std::vector<Ball> &balls) {
+	isInitialized_ = false;
+
+	ballsVec_ = balls;
+
+	ballsVec_.shrink_to_fit();
+	ballTrackers_.reserve(ballsVec_.size());
+}
+
+
+cv::Rect BallTracker::trackOne(unsigned short ballIndex, const cv::Mat &frame, bool callInit) {
+	cv::Rect bbox = ballsVec_[ballIndex].getBbox();
+
+	if (callInit) {
+		ballTrackers_[ballIndex]->init(frame, bbox);
+	}
+
+	bool updatedBbox = ballTrackers_[ballIndex]->update(frame, bbox);
+	ballsVec_[ballIndex].setBbox(bbox);
+	return bbox;
+
+//		std::cout<< "Ball " << ballIndex << " updated? " << updatedBbox << " current bbox: " << bbox << std::endl;
+
+//		return cv::Point(bbox.x + bbox.width / 2, bbox.y + bbox.height / 2);
+}
+
+
+std::vector<Ball> BallTracker::trackAll(const cv::Mat &frame) {
+	if (!isInitialized_) {
+		createTrackers();
+	}
+
+	for (unsigned short i = 0; i < ballsVec_.size(); i++) {
+		if (!isInitialized_){
+			trackOne(i, frame, true);
+		}
+		else {
+			trackOne(i, frame, false);
+		}
+	}
+
+	return ballsVec_;
+}
+

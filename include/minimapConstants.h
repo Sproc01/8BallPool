41498eb6e1968de083d9d52d85fe5d88e878d9e4
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2024-06-11 19:24:04 +0200

    Const for constant minimap
    If not multiple includes leads to error in linking

--- include/minimapConstants.h
+++ include/minimapConstants.h
@@ -7,21 +7,21 @@
 #include <opencv2/opencv.hpp>
 
 //path of the minimap
-std::string MINIMAP_PATH = "../img/minimap.png";
+const std::string MINIMAP_PATH = "../img/minimap.png";
 
 //corners of the table in the minimap
-cv::Point2f TOP_LEFT_MAP_CORNER = cv::Point2f(80, 78);
-cv::Point2f TOP_RIGHT_MAP_CORNER = cv::Point2f(886, 78);
-cv::Point2f BOTTOM_RIGHT_MAP_CORNER = cv::Point2f(886, 481);
-cv::Point2f BOTTOM_LEFT_MAP_CORNER = cv::Point2f(80, 481);
+const cv::Point2f TOP_LEFT_MAP_CORNER = cv::Point2f(80, 78);
+const cv::Point2f TOP_RIGHT_MAP_CORNER = cv::Point2f(886, 78);
+const cv::Point2f BOTTOM_RIGHT_MAP_CORNER = cv::Point2f(886, 481);
+const cv::Point2f BOTTOM_LEFT_MAP_CORNER = cv::Point2f(80, 481);
 
 //radius of the balls on the minimap
-int MAP_BALL_RADIUS = 12;
+const int MAP_BALL_RADIUS = 12;
 
 //colors for the balls in the minimap
-cv::Vec3d BLACK_BGR_COLOR = cv::Vec3d(0, 0, 0);
-cv::Vec3d WHITE_BGR_COLOR = cv::Vec3d(0, 0, 0);
-cv::Vec3d STRIPED_BGR_COLOR = cv::Vec3d(0, 255, 0);
-cv::Vec3d SOLID_BGR_COLOR = cv::Vec3d(0, 0, 255);
+const cv::Vec3d BLACK_BGR_COLOR = cv::Vec3d(0, 0, 0);
+const cv::Vec3d WHITE_BGR_COLOR = cv::Vec3d(0, 0, 0);
+const cv::Vec3d STRIPED_BGR_COLOR = cv::Vec3d(0, 255, 0);
+const cv::Vec3d SOLID_BGR_COLOR = cv::Vec3d(0, 0, 255);
 
 #endif //MINIMAPCONSTANTS_H

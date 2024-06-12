69fd1716f055819d0902ad9dabe972483600daf4
Michela Schibuola <michela.schibuola@gmail.com>
2024-06-12 20:45:47 +0200

    File util

--- src/transformation.cpp
+++ src/transformation.cpp
@@ -8,20 +8,6 @@
 using namespace cv;
 using namespace std;
 
-//TODO: it is duplicated, also in tableOrientation
-// Return the center point between two points
-Point2f getCenter2(Point2f p1, Point2f p2) {
-    Point2f center;
-    int dx_half = abs(p1.x - p2.x)/2;
-    int dy_half = abs(p1.y - p2.y)/2;
-    if(p1.x > p2.x)
-        dx_half *= -1;
-    if(p1.y > p2.y)
-        dy_half *= -1;
-    center = Point2f(p1.x + dx_half, p1.y + dy_half);
-    return center;
-}
-
 Mat imgWithTransform(Mat frame, Mat transform, Table table) {
     Vec<Point2f, 4> img_vertices = table.getBoundaries();
 

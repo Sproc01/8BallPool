a06f833ff8f21e4dc4adab2687dc5299c068bb31
Michela Schibuola <michela.schibuola@gmail.com>
2024-07-21 12:16:32 +0200

    Ball documentation

--- include/ball.h
+++ include/ball.h
@@ -6,31 +6,98 @@
 #include <opencv2/core/types.hpp>
 #include "category.h"
 
+/**
+ * Implementation of a ball.
+ *
+ * The class contains the information about a ball: the position, the category, the visibility
+ * and the position of the same ball in a previous frame.
+ */
 class Ball {
 	cv::Rect bbox_;
 	Category category_;
 	cv::Rect bbox_prec_;
 	bool visible_;
 
-	public:
-		Ball(cv::Rect bbox, Category category, cv::Rect bbox_prec, bool visible = true) : bbox_(bbox), category_(category),
-		                                                             bbox_prec_(bbox_prec), visible_(visible) {
-		}
-		Ball(cv::Rect bbox, Category category, bool visible = true) : bbox_(bbox), category_(category),
-								bbox_prec_(cv::Rect(-1, -1, -1, -1)), visible_(visible) {
-		}
+public:
+	/**
+	* @brief Constructor of ball when current and previous positions are known.
+	* @param bbox bbox of the ball.
+	* @param category category of the ball.
+	* @param bbox_prec bbox_prec of the ball.
+	* @param visible visibility of the ball.
+	*/
+	Ball(cv::Rect bbox, Category category, cv::Rect bbox_prec, bool visible = true) : bbox_(bbox), category_(category), bbox_prec_(bbox_prec), visible_(visible) {}
+	/**
+	* @brief Constructor of ball when just the current is known.
+	* @param bbox bbox of the ball.
+	* @param category category of the ball.
+	* @param visible visibility of the ball.
+	*/
+	Ball(cv::Rect bbox, Category category, bool visible = true) : bbox_(bbox), category_(category),bbox_prec_(cv::Rect(-1, -1, -1, -1)),visible_(visible) {}
 
-		cv::Rect getBbox() const;
-		Category getCategory() const;
-		cv::Rect getBbox_prec() const;
-		cv::Point2f getBBoxCenter() const;
-		cv::Point2f getBboxCenter_prec() const;
-		bool getVisibility() const;
+	/**
+	* @brief Return the rectangle containing the ball.
+	* @return the bbox of the ball.
+	* @throw runtime_error if the bbox is empty.
+	*/
+	cv::Rect getBbox() const;
 
-		void setBbox(cv::Rect bbox);
-		void setCategory(Category category);
-		void setBbox_prec(cv::Rect bbox_prec);
-		void setVisibility(bool visible);
+	/**
+	 * @brief Return the rectangle containing the ball in the previous frame.
+	 * @return the bbox of the ball in the previous frame.
+	 * @throw runtime_error if the bbox is empty.
+	 */
+	cv::Rect getBbox_prec() const;
+
+	/**
+	* @brief Return the category of the ball.
+	* @return the category of the ball.
+	* @throw runtime_error if the category does not correspond to a ball.
+	*/
+	Category getCategory() const;
+
+	/**
+	 * @brief Return the center of the rectangle containing the ball.
+	 * @return the center of the bbox of the ball.
+	 */
+	cv::Point2f getBBoxCenter() const;
+
+	/**
+	 * @brief Return the center of the rectangle containing the ball in the previous frame.
+	 * @return the center of the bbox of the ball in the previous frame.
+	 */
+	cv::Point2f getBboxCenter_prec() const;
+
+	/**
+	 * @brief Return if the ball is inside the table area.
+	 * @return true if the ball is inside the table area, false otherwise.
+	 */
+	bool getVisibility() const;
+
+	/**
+	 * @brief Set a value to the rectangle of the ball.
+	 * @param bbox the new bbox position.
+	 */
+	void setBbox(cv::Rect bbox);
+
+	/**
+	 * @brief Set a value to the rectangle of the ball in the previous frame.
+	 * @param bbox_prec the new bbox position of the previous frame.
+	 */
+	void setBbox_prec(cv::Rect bbox_prec);
+
+	/**
+	 * @brief Set a value to the category of the ball.
+	 * @param category the new category.
+	 * @throw runtime_error if the category does not correspond to a ball.
+	 */
+	void setCategory(Category category);
+
+	/**
+	 * @brief Set a value to the visibility of the ball.
+	 * @param visible the new visibility value.
+	 */
+	void setVisibility(bool visible);
 };
 
 #endif // BALL_H
--- src/ball.cpp
+++ src/ball.cpp
@@ -7,6 +7,11 @@
 
 using namespace cv;
 
+/**
+ * @brief Return the rectangle containing the ball.
+ * @return the bbox of the ball.
+ * @throw runtime_error if the bbox is empty.
+ */
 Rect Ball::getBbox() const {
 	if (bbox_.empty())
 		throw std::runtime_error("bbox is uninitialized");
@@ -14,44 +19,86 @@ Rect Ball::getBbox() const {
 	return bbox_;
 }
 
+/**
+ * @brief Return the rectangle containing the ball in the previous frame.
+ * @return the bbox of the ball in the previous frame.
+ * @throw runtime_error if the bbox is empty.
+ */
+Rect Ball::getBbox_prec() const {
+    if (bbox_prec_.empty())
+        throw std::runtime_error("bbox_prec is uninitialized");
+
+    return bbox_prec_;
+}
+
+/**
+ * @brief Return the category of the ball.
+ * @return the category of the ball.
+ * @throw runtime_error if the category does not correspond to a ball.
+ */
 Category Ball::getCategory() const {
-	if (category_ == 0)
-		throw std::runtime_error("category is uninitialized");
+	if (category_ == 0 || category_ == 5)
+		throw std::runtime_error("category does not correspond to a ball");
 
 	return category_;
 }
 
-Rect Ball::getBbox_prec() const {
-	if (bbox_prec_.empty())
-		throw std::runtime_error("bbox_prec is uninitialized");
-
-	return bbox_prec_;
-}
-
+/**
+ * @brief Return the center of the rectangle containing the ball.
+ * @return the center of the bbox of the ball.
+ */
 Point2f Ball::getBBoxCenter() const {
 	return Point(bbox_.x + bbox_.width / 2, bbox_.y + bbox_.height / 2);
 }
 
+/**
+ * @brief Return the center of the rectangle containing the ball in the previous frame.
+ * @return the center of the bbox of the ball in the previous frame.
+ */
 Point2f Ball::getBboxCenter_prec() const {
 	return Point(bbox_prec_.x + bbox_prec_.width / 2, bbox_prec_.y + bbox_prec_.height / 2);
 }
 
+/**
+ * @brief Return if the ball is inside the table area.
+ * @return true if the ball is inside the table area, false otherwise.
+ */
 bool Ball::getVisibility() const {
 	return visible_;
 }
 
+/**
+ * @brief Set a value to the rectangle of the ball.
+ * @param bbox the new bbox position.
+ */
 void Ball::setBbox(Rect bbox) {
 	bbox_ = bbox;
 }
 
-void Ball::setCategory(Category category) {
-	category_ = category;
-}
-
+/**
+ * @brief Set a value to the rectangle of the ball in the previous frame.
+ * @param bbox_prec the new bbox position of the previous frame.
+ */
 void Ball::setBbox_prec(Rect bbox_prec) {
 	bbox_prec_ = bbox_prec;
 }
 
+/**
+ * @brief Set a value to the category of the ball.
+ * @param category the new category.
+ * @throw runtime_error if the category does not correspond to a ball.
+ */
+void Ball::setCategory(Category category) {
+	if (category_ == 0 || category_ == 5)
+		throw std::runtime_error("category does not correspond to a ball");
+
+	category_ = category;
+}
+
+/**
+ * @brief Set a value to the visibility of the ball.
+ * @param visible the new visibility value.
+ */
 void Ball::setVisibility(bool visible) {
 	visible_ = visible;
 }
